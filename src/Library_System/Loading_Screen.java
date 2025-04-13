0e8a8672d8eab950d9ef9150109653b95d99b338
Jhonmark <123848619+CodeByJhonmark@users.noreply.github.com>
2025-04-14 02:36:39 +0800

    updated

--- src/Library_System/Loading_Screen.java
+++ src/Library_System/Loading_Screen.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Library_System;
 
-/**
- *
- * @author payno
- */
+//@author payno
+ 
 public class Loading_Screen extends javax.swing.JFrame {
 
     /**
